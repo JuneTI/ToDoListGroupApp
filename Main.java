cb0c6832356aaa4637e5d06739796dc00f4910ff
JuneTI <76265513+JuneTI@users.noreply.github.com>
2021-11-08 21:19:41 -0600

    Updated Main.java
    Added Center Pane that helps showcase the task's due date, description, category and title

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,6 +15,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.collections.*;
 
 
 public class Main extends Application {
@@ -32,6 +35,13 @@ public class Main extends Application {
 		//Left side (Display List)
 		toDo toDoList = new toDo();
 		ListView lv = new ListView();
+		lv.setItems(FXCollections.observableArrayList(toDoList.tasks));
+		lv.setMaxWidth(200);
+		lv.setPrefHeight(650);
+		lv.setMaxHeight(650);
+		Label listOfTasks = new Label("List of Tasks: ");
+		listOfTasks.setFont(new Font("Arial", 15));
+		listOfTasks.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
 		
 		//Bottom (Sort buttons)
 		Label buttonsLabel = new Label("Sort by:");
@@ -42,7 +52,6 @@ public class Main extends Application {
 		Button category = new Button("Category");
 		Button priority = new Button("Priority");
 
-		
 		//Constructing right side
 		VBox vbRight = new VBox();
 		vbRight.setId("rightSide");
@@ -51,10 +60,55 @@ public class Main extends Application {
 		vbRight.getChildren().addAll(labelAddTaskField, userAddTaskField, bt, removebt);
 		
 		//Constructing left side
+		VBox vbLeft = new VBox();
+		vbLeft.setId("leftSide");
+		vbLeft.setPadding(new Insets(50.00, 0, 0, 20.00));
+		vbLeft.getChildren().addAll(listOfTasks, lv);
+		
+		//Constructing the center of pane that contains the details of the task
 		VBox vbCenter = new VBox();
 		vbCenter.setId("center");
-		vbCenter.setPadding(new Insets(50.00, 0, 0, 20.00));
-		vbCenter.getChildren().addAll(lv);
+		vbCenter.setPadding(new Insets(50, 20, 0 ,35));
+		HBox centerHB1 = new HBox();
+		centerHB1.setAlignment(Pos.BASELINE_CENTER);
+		centerHB1.setSpacing(150);
+		HBox centerHB2 = new HBox();
+		centerHB2.setAlignment(Pos.BASELINE_CENTER);
+		centerHB2.setSpacing(150);
+		HBox centerHB3 = new HBox();
+		centerHB3.setAlignment(Pos.TOP_LEFT);
+		Label taskNameLabel = new Label("Task Name: ");
+		taskNameLabel.setFont(new Font("Arial", 15));
+		taskNameLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
+		Text taskNameText = new Text();
+		taskNameText.setFont(new Font("Arial", 15));
+		taskNameText.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
+		Label dueDateLabel = new Label("Due Date: ");
+		dueDateLabel.setFont(new Font("Arial", 15));
+		dueDateLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
+		Text dueDateText = new Text();
+		dueDateText.setFont(new Font("Arial", 15));
+		dueDateText.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
+		dueDateText.setTextAlignment(TextAlignment.RIGHT);
+		dueDateLabel.setTextAlignment(TextAlignment.RIGHT);
+		
+		Label taskDescLabel = new Label("Task Description");
+		taskDescLabel.setFont(new Font("Arial", 15));
+		taskDescLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
+		TextArea taskDescriptionText = new TextArea();
+		taskDescriptionText.setPrefHeight(630);
+		taskDescriptionText.setPrefWidth(600);
+		Label categLabel = new Label("Category: ");
+		categLabel.setFont(new Font("Arial", 15));
+		categLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
+		Text categText = new Text();
+		categText.setFont(new Font("Arial", 15));
+		categText.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
+		
+		centerHB1.getChildren().addAll(taskNameLabel, taskNameText, dueDateLabel, dueDateText);
+		centerHB2.getChildren().addAll(taskDescLabel, categLabel, categText);
+		centerHB3.getChildren().addAll(taskDescriptionText);
+		vbCenter.getChildren().addAll(centerHB1, centerHB2, centerHB3);
 		
 		//Constructing bottom
 		HBox hb = new HBox(80);
@@ -66,6 +120,7 @@ public class Main extends Application {
 		//Constructing full BorderPane/root/scene
 		BorderPane root = new BorderPane();
 		root.setCenter(vbCenter);
+		root.setLeft(vbLeft);
 		root.setRight(vbRight);
 		root.setBottom(hb);
 		Scene scene = new Scene(root,1200,800);
